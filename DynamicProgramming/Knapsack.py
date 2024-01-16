41f4ba1324f0d50e7e66d1ed328f9f1b4cfaaa52
Wounsuk Rhee <43737862+rhee1998@users.noreply.github.com>
2024-01-16 11:43:45 +0900

    Update Knapsack.py

--- DynamicProgramming/Knapsack.py
+++ DynamicProgramming/Knapsack.py
@@ -18,6 +18,5 @@ def Knapsack(WMAX, W, V):
                 curr[wmax] = prev[wmax]
             else:
                 curr[wmax] = max([prev[wmax], prev[wmax - W[i]] + V[i]])
-
         prev, curr = curr, [0 for _ in range(WMAX + 1)]
     return prev[-1]
