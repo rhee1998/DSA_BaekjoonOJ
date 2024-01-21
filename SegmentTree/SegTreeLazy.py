7e132f762d3c9d340bac55b91baccdb9e6717c48
Wounsuk Rhee <43737862+rhee1998@users.noreply.github.com>
2024-01-21 14:34:28 +0900

    Create SegTreeLazy.py

---
+++ SegmentTree/SegTreeLazy.py
@@ -0,0 +1,84 @@
+##############################################
+##### Segment Tree with Lazy Propagation #####
+##############################################
+
+import sys, math
+from collections import deque
+input = sys.stdin.readline
+
+TREE, RANG, LAZY = [], [], []
+
+# Build Segment Tree
+def Build(A, default=0):
+    N = 1 << (math.ceil(math.log2(len(A))))
+    A += [default] * (N - len(A))
+
+    TREE = [default for _ in range(2 * N)]
+    RANG = [0 for _ in range(2 * N)]
+    LAZY = [0 for _ in range(2 * N)]
+
+    for i in range(N):
+        TREE[N + i] = A[i]
+        RANG[N + i] = (i, i)
+
+    for i in range(N - 1, 0, -1):
+        TREE[i] = TREE[i << 1] + TREE[(i << 1) | 1]
+        RANG[i] = (RANG[i << 1][0], RANG[(i << 1) | 1][1])
+
+    return TREE, RANG, LAZY
+
+# Update Lazy
+def UpdateLazy(node):
+    if LAZY[node] != 0:
+        S, E = RANG[node]
+        TREE[node] += LAZY[node] * (E - S + 1)
+        if S != E:
+            LAZY[node << 1] += LAZY[node]
+            LAZY[(node << 1) | 1] += LAZY[node]
+        LAZY[node] = 0
+
+    return None
+
+# Modify Range : Add val to A[L], A[L + 1], ..., A[R]
+def UpdateRange(L, R, val):
+    que, back = deque([1]), []
+    while que:
+        node = que.popleft()
+        S, E = RANG[node]; UpdateLazy(node)
+        
+        if L > E or R < S:
+            continue
+
+        if L <= S and E <= R:
+            TREE[node] += (E - S + 1) * val
+            if S != E:
+                LAZY[node << 1] += val
+                LAZY[(node << 1) | 1] += val
+            continue
+
+        que.append((node << 1))
+        que.append((node << 1) | 1)
+        back.append(node)
+    
+    while back:
+        node = back.pop()
+        try: TREE[node] = TREE[(node << 1)] + TREE[(node << 1) | 1]
+        except: continue
+
+    return None
+
+# Query : Sum of A[L], A[L + 1], ..., A[R]
+def Query(L, R):
+    res = 0
+    que = deque([1])
+    while que:
+        node = que.popleft()
+        S, E = RANG[node]; UpdateLazy(node)
+
+        if L > E or R < S: continue
+        if L <= S and E <= R: res += TREE[node]; continue
+
+        que.append((node << 1))
+        que.append((node << 1) | 1)
+
+    return res
