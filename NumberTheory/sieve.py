61b03bf0e5003ce846930b9fbaa4fb8cfa66eee2
Wounsuk Rhee <43737862+rhee1998@users.noreply.github.com>
2024-02-13 10:59:59 +0900

    Create sieve.py

---
+++ NumberTheory/sieve.py
@@ -0,0 +1,21 @@
+# ===================== #
+# Sieve of Eratosthenes #
+# ===================== #
+
+def Sieve(N):
+    # ==================================================
+    # Input(s)
+    # - N        : maximum number to study primality
+    #
+    # Output(s)
+    # - is_prime : is_prime[idx] = 1 if idx is prime, otherwise 0
+    # ==================================================
+    
+    is_prime = [0, 0] + [1] * (N - 1)
+    
+    for i in range(2, MAX + 1):
+        if is_prime[i] == 1:
+            for j in range(i * 2, MAX + 1, i):
+                is_prime[j] = 0
+
+    return is_prime
