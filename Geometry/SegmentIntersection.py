1d84048196004eda556072eeba7142b32b979cab
Wounsuk Rhee <43737862+rhee1998@users.noreply.github.com>
2024-02-27 15:51:08 +0900

    Update SegmentIntersection.py

--- Geometry/SegmentIntersection.py
+++ Geometry/SegmentIntersection.py
@@ -18,6 +18,7 @@ def CCW(A, B, C):
     AB = (B[0] - A[0], B[1] - A[1])
     BC = (C[0] - B[0], C[1] - B[1])
     res = AB[0] * BC[1] - AB[1] * BC[0]
+    
     if res > 0: return 1
     if res < 0: return -1
     return 0
