06af1008b6710787f69ddae655542748cca1c5a5
Wounsuk Rhee <43737862+rhee1998@users.noreply.github.com>
2024-08-04 01:08:41 +0900

    Update GreaterThanK.py

--- Sorting/GreaterThanK.py
+++ Sorting/GreaterThanK.py
@@ -15,19 +15,3 @@ def GreaterThanK(A, K):
             S, E = M + 1, E
     if A[S] > K: res += 1
     return res
-
-def GreaterOrEqualToX(A, x):
-    # A should be sorted in ascending order
-    if x > A[-1]: return 0
-    if x <= A[0]: return len(A)
-
-    res = 0
-    S, E = 0, len(A) - 1
-    while S < E:
-        M = (S + E) // 2
-        if A[M] > x:
-            res += E - M + 1
-            S, E = S, M - 1
-        else:
-            S, E = M, E
-    return res
