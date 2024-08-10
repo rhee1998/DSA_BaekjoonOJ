44524fcf350a13d500be633484857179998dc6e6
Wounsuk Rhee <43737862+rhee1998@users.noreply.github.com>
2024-08-10 18:13:50 +0900

    Update GCD.py

--- NumberTheory/GCD.py
+++ NumberTheory/GCD.py
@@ -4,10 +4,7 @@
 
 def GCD(x, y):
     x, y = abs(x), abs(y)
-    if x < y:
-        x, y = y, x
-    
-    while y > 0:
-        x, y = y, x % y
+    if x < y: x, y = y, x
+    while y > 0: x, y = y, x % y
     
     return x
